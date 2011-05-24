51066f1ede2de4735193c2578d3b1d6747a322ec
Miles Z. Sterrett <miles@mileszs.com>
2011-05-24 14:48:44 -0400

    Add some sections, and scrollTo

--- js/script.js
+++ js/script.js
@@ -1,12 +1,30 @@
 /* Author: Miles Z. Sterrett */
 
 $(function() {
+  // TODO: This needs to pull existing style info to reset, instead of
+  // forcing me to change style info in two fucking places.
   $('.screenshot').hover(function() {
-    $(this).find('.screenshot-info').css('height', '400px');
-    $(this).find('.screenshot-top').animate({height: '80px'}, 500);
+    $(this).find('.screenshot-info').css('height', '240px');
+    $(this).find('.screenshot-top').animate({height: '60px'}, 500);
   }, function() {
-    $(this).find('.screenshot-top').animate({height: '400px'}, 500, function() {
-      $(this).find('.screenshot-info').css('height', '80px');
+    $(this).find('.screenshot-top').animate({height: '240px'}, 500, function() {
+      $(this).find('.screenshot-info').css('height', '60px');
     });
+  });
+
+  // TODO: clearly the following bullshit can be abstracted
+  $('a.projects-link').click(function(ev) {
+    ev.preventDefault();
+    $.scrollTo('#projects', 1000);
+  });
+
+  $('a.contact-link').click(function(ev) {
+    ev.preventDefault();
+    $.scrollTo('#contact', 1000);
+  });
+
+  $('a.hello-link').click(function(ev) {
+    ev.preventDefault();
+    $.scrollTo('#hello', 1000);
   });
 });
